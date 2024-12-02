3117cdb978919a3a28d917dd88d86b1fa654ba46
jmakkie <jaapimakkonen13@gmail.com>
2024-12-03 01:59:29 +0200

    data is loaded to tableview in arvot when tab is changed

--- MateriaaliLaskin/mainwindow.cpp
+++ MateriaaliLaskin/mainwindow.cpp
@@ -15,17 +15,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+// laske/calculate/mainwindow
 void MainWindow::on_LaskeButton_clicked()
 {
     ui->tabWidget->setCurrentIndex(1);
 }
 
+
+
 // values/arvot page functions
+void MainWindow::on_tabWidget_currentChanged(int index)
+{
+    QString selectedData = "lisat";
+    // change dbconn to be a passable/global variable
+    dbconnection dbconn;
+
+    //have function return a model
+    QSqlQueryModel* model = loadDataToQtableView(dbconn, index, selectedData);
+    // update view to show selected table
+    ui->arvotTableView->setModel(model);
+}
+
 void MainWindow::on_arvotAddNewButton_clicked()
 {
-
     QString table, name, value;
-    // add check for which table
+
+    // add check for which table from selected in the qtableview
     table = "Lisat";
 
     name = ui -> arvotMateriaaliLineEdit -> text();
@@ -34,9 +50,22 @@ void MainWindow::on_arvotAddNewButton_clicked()
     qDebug() << name << value;
 
     // add checks if data in name and value are correct format etc.
+
+    // change dbconn to be a passable/global variable
     dbconnection dbconn;
     QString result = saveData(dbconn, table, name, value);
 
     ui -> label_6 ->setText(result);
 }
 
+
+void MainWindow::on_arvotdeleteButton_clicked()
+{
+    //implement deleting
+}
+
+
+void MainWindow::on_arvotUpdateButton_clicked()
+{
+    //implement updating
+}
